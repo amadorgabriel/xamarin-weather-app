926ec35c1de86b318af98243759cab42f38f4190
FIC <gabrielramador2014@gmail.com>
2020-11-14 19:23:27 -0200

    core da implementação com api concluida

--- WeatherApp/Views/CurrentWeatherPage.xaml.cs
+++ WeatherApp/Views/CurrentWeatherPage.xaml.cs
@@ -25,7 +25,7 @@ namespace WeatherApp.Views
 
         private async void GetWeatherInfo()
         {
-            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid={ApiKey}";
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid={ApiKey}&units=metric";
 
             var result = await ApiCaller.Get(url);
 
@@ -46,19 +46,69 @@ namespace WeatherApp.Views
                     var dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
                     dateTxt.Text = dt.ToString("dddd, MM, dd").ToUpper();
 
+                    GetForecast();
+                    d
                 }catch(Exception exe)
                 {
-                    //ADD TRATAMENTO
+                    await DisplayAlert("Weather Info", exe.Message, "OK");
+
                 }
+            }
+            else
+            {
+                await DisplayAlert("Weather Info", "No weather information found", "OK");
             }
         }
 
         private async void GetForecast()
         {
-            var url = $"http://api.openweathermap.org/data/2.5/forecast?q={Location}&appid={ApiKey}";
+            var url = $"http://api.openweathermap.org/data/2.5/forecast?q={Location}&appid={ApiKey}&units=metric";
             var result = await ApiCaller.Get(url);
 
+            if (result.Sucessful)
+            {
+                try
+                {
+                    var forecastInfo = JsonConvert.DeserializeObject<ForecastInfo>(result.Response);
+                    List<List> allList = new List<List>();
 
+                    foreach(var list in forecastInfo.list)
+                    {
+                        var date = DateTime.Parse(list.dt_txt);
+
+                        if(date > DateTime.Now && date.Hour == 0 && date.Minute == 0 && date.Second == 0)
+                            allList.Add(list);
+                    }
+
+                    dayOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dddd");
+                    dateOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dd MM");
+                    iconOneImg.Source = $"w{allList[0].weather[0].icon}";
+                    tempOneTxt.Text = allList[0].main.temp.ToString("0");
+
+                    dayTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dddd");
+                    dateTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dd MM");
+                    iconTwoImg.Source = $"w{allList[1].weather[0].icon}";
+                    tempTwoTxt.Text = allList[1].main.temp.ToString("0");
+
+                    dayThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dddd");
+                    dateThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dd MM");
+                    iconThreeImg.Source = $"w{allList[2].weather[0].icon}";
+                    tempThreeTxt.Text = allList[2].main.temp.ToString("0");
+
+                    dayFourTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dddd");
+                    dateFourTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dd MM");
+                    iconFourImg.Source = $"w{allList[3].weather[0].icon}";
+                    tempFourTxt.Text = allList[3].main.temp.ToString("0");
+
+                }catch(Exception exe)
+                {
+                    await DisplayAlert("Forecast Info", exe.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Forecast Info", "No weather information found", "OK");
+            }
         }
 
     }
